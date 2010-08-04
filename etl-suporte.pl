5d91622ac2b65735c4e3672663ae17cd2ff83428
Daniel Ruoso <daniel@ruoso.com>
2010-08-04 14:07:32 -0300

    tem o historico dos tickets montados e um esboco da tabela de fato.

--- etl-suporte.pl
+++ etl-suporte.pl
@@ -17,8 +17,16 @@ my %nomes_filas;
 };
 my %ids_filas = reverse %nomes_filas;
 
-my $id_minimo = 0;
-my $id_maximo = 999999;
+my $id_minimo = 421;
+my $id_maximo = 421;
+my $data_minima = '-infinity';
+my $data_maxima = 'infinity';
+
+my $sth_historico_completo = $dbi->prepare
+  ('SELECT * FROM transactions WHERE '.
+   'objecttype=? AND objectid=? '.
+   'order by created DESC');
+
 
 {   # vamos pasar por todos os tickets.
     # precisamos passar por todos, porque não sabemos se ele, em algum
@@ -31,7 +39,7 @@ my $id_maximo = 999999;
        'created BETWEEN ? AND ? AND '.
        'id BETWEEN ? AND ? '.
        'ORDER BY id');
-    $sth_tickets->execute('2010-07-01 00:00:00', '2010-07-31 23:59:59',
+    $sth_tickets->execute($data_minima, $data_maxima,
                           $id_minimo, $id_maximo);
     my $sth_historico = $dbi->prepare
       ('SELECT oldvalue FROM transactions WHERE '.
@@ -70,13 +78,10 @@ sub fila_interessa {
 }
 
 
-my $sth_historico_completo = $dbi->prepare
-  ('SELECT * FROM transactions WHERE '.
-   'objecttype=? AND objectid=? '.
-   'order by created DESC');
 sub processar_ticket_suporte {
     my $ticket = shift;
     my @historico;
+    $sth_historico_completo->execute('RT::Ticket', $ticket->{id});
     while (my $t = $sth_historico_completo->fetchrow_hashref) {
         push @historico, $t;
     }
@@ -84,23 +89,61 @@ sub processar_ticket_suporte {
     # vamos pegar as alterações que são feitas todas de uma vez e
     # consolidar (usando o campo created);
     my @historico_consolidado;
-    my %alteracoes;
+    my $alteracoes = {};
     my $last_created = 0;
     foreach my $t (@historico) {
         if ($last_created ne $t->{created}) {
             push @historico_consolidado,
-              { created => $last_created;
-                alteracoes => \%alteracoes };
-            %alteracoes = ();
+              { created => $last_created,
+                alteracoes => $alteracoes };
+            $alteracoes = {};
             $last_created = $t->{created};
         }
-        $alteracoes{lc($t->{field})} = $t->{oldvalue};
+        $alteracoes->{lc($t->{field})} = $t->{oldvalue};
     }
 
     # Agora vamos fazer uma lista consolidada das versões dos tickets
     # incluindo o periodo quando ele foi valido.
+    my $working_ticket = $ticket;
     my @versoes = ( $ticket ); # o primeiro elemento é a versão recente
+    foreach my $patch (@historico_consolidado) {
+        $working_ticket->{transaction_time} = $patch->{created};
+        $working_ticket = { %$working_ticket, %{$patch->{alteracoes}} };
+        push @versoes, $working_ticket;
+    }
+    @versoes = reverse @versoes;
 
-    
+
+    # Processo de Suporte
+    # Ticket chega no primeiro nivel como "new" e sem proprietario
+    # Alguém toma o ticket e abre "open".
+    # Pode ser resolvido no primeiro nivel.
+    # Ou então repassa para outra fila (2o. nivel)
+    # Alguém toma o ticket no segundo nivel.
+    # Pode ser resolvido no segundo nivel.
+    # Ou ficar pendente.
+    # Então é resolvido no Terceiro Nivel.
+
+    # D datahora_requisicao
+    # D datahora_inicio_atendimento
+    # D atendente no primeiro nivel
+    # D fila_segundo_nivel
+    # D datahora_escala_segundo_nivel
+    # D datahora_inicio_atendimento_segundo_nivel
+    # D atendente no segundo nivel
+    # D datahora_escala_terceiro_nivel
+    # D datahora_resolucao
+    # M minutos_uteis_de_espera_para_inicio
+    # M minutos_corridos_de_espera_para_inicio
+    # M minutos_uteis_encaminhamento_primeiro_nivel
+    # M minutos_corridos_encaminhamento_primeiro_nivel
+    # M minutos_uteis_de_espera_segundo_nivel
+    # M minutos_corridos_de_espera_segundo_nivel
+    # M minutos_uteis_encaminhamento_segundo_nivel
+    # M minutos_corridos_encaminhamento_segundo_nivel
+    # M minutos_uteis_pendencia_terceiro_nivel
+    # M minutos_corridas_pendencia_terceiro_nivel
+    # M minutos_uteis_total_atendimento
+    # M minutos_corridos_total_atendimento
 
 }
